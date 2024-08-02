db1a1b81bf3be4996d05aa5d67e1ffda161a7275
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-02 15:10:57 +0530

    [sidenav]

--- react-side-navigation-bar-1/src/App.tsx
+++ react-side-navigation-bar-1/src/App.tsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-     
+     <div className="sidenav">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+      </div>
     </>
   )
 }
