3eb056ecce75911fa1098208a513cff67801bf4b
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-02 12:43:50 +0530

    [css] variables and theme

--- react-navbar-css-variables-6/src/App.tsx
+++ react-navbar-css-variables-6/src/App.tsx
@@ -1,10 +1,28 @@
-import './App.css';
+// import './App.css';
+import './theme/theme.css';
+import { useState } from 'react';
 
 function App() {
+  // State to keep track of the active tab
+  const [activeTab, setActiveTab] = useState<string>('#home');
+  const [activeTheme, setActiveTheme] = useState<string>('dark-mode');
+  const [isResponsive, setIsResponsive] = useState(false);
+ 
+  // Function to handle tab click
+  const tabClickHandler = (tab: string) => {
+    setActiveTab(tab);
+  };
+ 
+  const themeHandler = () => {
+    setActiveTheme((prevTheme) => (prevTheme === 'dark-mode' ? 'light-mode' : 'dark-mode'));
+  }
 
   return (
     <>
-      
+      <div className={activeTheme} style={{ width: "100vw", height: "50vh" }}>
+        <h1>Hello World</h1>
+      </div>
+      <button onClick={themeHandler}>Theme</button>
     </>
   )
 }
