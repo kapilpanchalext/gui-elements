f34513593340d45f03a1d701f1857d35f96a3719
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-05 08:07:30 +0530

    [component] added navbar theme

--- react-navbar-optimization-9/src/App.tsx
+++ react-navbar-optimization-9/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1>Navbar Optimization 1</h1>
+      <Navbar />
     </>
   )
 }
