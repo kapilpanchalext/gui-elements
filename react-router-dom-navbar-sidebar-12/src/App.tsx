84a0d5d1ea616fdeca65749e52c604d6faf37be4
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-05 14:32:31 +0530

    [router] added components

--- react-router-dom-navbar-sidebar-12/src/App.tsx
+++ react-router-dom-navbar-sidebar-12/src/App.tsx
@@ -1,11 +1,32 @@
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import RootLayout from './components/layout/RootLayout';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout/>,
+      errorElement: '',      
+      children: [
+        {
+          path: '/',
+          element: <Navigate to="/dashboard" replace />,
+        },
+        {
+          path: '/dashboard',
+          element: <Dashboard/>
+        },
+      ]
+    },
+  ]);
+
   return (
-    <>
-      
-    </>
+    <div>
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
---
+++ react-router-dom-navbar-sidebar-12/src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import "../sidebar/Sidebar.css";
+import "../../../theme/theme.css";
+import { SidebarData } from './utils/SidebarData';
+
+const Sidebar = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  // Toggle the expanded state
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  // const closeSidebarHandler = () => {
+  //   if(isExpanded === true){
+  //     setIsExpanded(false);
+  //   }
+  // }
+
+  return (
+    <>
+      <div style={{ display: "flex", height: "100vh" }}>
+        <div className={`sidenav ${isExpanded ? 'expanded' : ''}`} onDoubleClick={toggleSidebar}>
+          <div className="content">
+            {SidebarData.map((item) => {
+              let toRender;
+
+              if (item.tabName.startsWith("separator")) {
+                toRender = <div key={item.link} className="separator"></div>
+              } else {
+                toRender = <a key={item.link} href={item.link}><span className="material-symbols-outlined">{item.icon}</span><span className="text">{item.tabName}</span></a>
+              }
+
+              return toRender;
+            })}
+          </div>
+          <div className="bottom-content">
+            <div className="separator"></div>
+            <a href="#envelope"><span className="material-symbols-outlined">account_circle</span><span className="text">UserName</span></a>
+          </div>
+        </div>
+
+        <div className="main" style={{ overflow: "auto", flex: 1, height: "100vh", backgroundColor:"var(--primary-100)", color:"var(--secondary-900)" }}>
+          <h1>Main Content Area</h1>
+          <p>Your content goes here...</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;
