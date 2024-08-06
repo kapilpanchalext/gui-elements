38e7a28e57b42e0f162e71c4c0b0f6c9288a1d42
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-06 11:06:05 +0530

    [router] navigation

---
+++ react-navigation-breadcrumb-router-dom-17/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div style={{height: "100%", display: "flex", backgroundColor: "transparent"}}>
+        Dashboard
+    </div>
+  )
+}
+
+export default Dashboard;
