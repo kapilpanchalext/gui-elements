97ae535197f214ce071a691219d2d238b933c2f4
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-08 11:06:20 +0530

    [useContext]

--- react-navbar-theme-23/src/App.tsx
+++ react-navbar-theme-23/src/App.tsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import "./theme/theme.css";
+import { ActiveThemeContext } from "./store/ActiveThemeContext";
 
 function App() {
+  const {theme, toggleTheme} = useContext(ActiveThemeContext);
 
   return (
     <>
+      <div data-app-theme={theme} style={{ height: "500px", width: "1000px"}}>
+        <button onClick={toggleTheme}>Change Theme</button>
+      </div>
     </>
   )
 }
