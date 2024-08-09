b7aa4e374386ca253e07f7b57fad9b9906e6591c
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-09 13:45:44 +0530

    [breadcrumb]

--- react-breadcrumb-router-15/src/App.tsx
+++ react-breadcrumb-router-15/src/App.tsx
@@ -43,7 +43,6 @@ function App() {
 
   return (
     <div>
-      
       <RouterProvider router={router}/>
     </div>
   )
--- react-navigation-breadcrumb-28/src/components/rootlayout/RootLayout.tsx
+++ react-navigation-breadcrumb-28/src/components/rootlayout/RootLayout.tsx
@@ -2,12 +2,14 @@ import Navbar from '../navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import "./RootLayout.css";
 import Sidebar from '../sidebar/Sidebar';
+import Breadcrumb from '../breadcrumb/Breadcrumb';
 
 const RootLayout = () => {
   return (
     <>
       <header>
         <Navbar />
+        <Breadcrumb />
       </header>
       <aside className="sidebar">
         <Sidebar />
