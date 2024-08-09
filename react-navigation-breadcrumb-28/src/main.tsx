c04be7603280dddf7cc5a1bcd53275db46d79616
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-09 12:56:16 +0530

    [breadcrumb] context hook

--- react-navigation-breadcrumb-28/src/main.tsx
+++ react-navigation-breadcrumb-28/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ActiveThemeProvider from './store/ActiveThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <ActiveThemeProvider>
+      <App />
+    </ActiveThemeProvider>
   </StrictMode>,
 )
