c73104a6d178d0e663e18ae970b1055d9e56fefb
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-09 12:07:42 +0530

    [layout] navbar sidebar theme

--- react-layout-navbar-theme-26/src/components/sidebar/Sidebar.tsx
+++ react-layout-navbar-theme-26/src/components/sidebar/Sidebar.tsx
@@ -1,16 +1,41 @@
 import { Link } from 'react-router-dom';
 import "./Sidebar.css";
+import { useState } from 'react';
+import { SidebarData } from './utils/SidebarData';
 
 
 const Sidebar = () => {
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+    const toggleSidebar = () => {
+        setIsExpanded(!isExpanded);
+    };
+
   return (
-    <div className='sidenav'>
-        <ul>
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
-    </div>
+    <>
+      <div style={{ display: "flex", height: "100vh" }}>
+        <div className={`sidenav ${isExpanded ? 'expanded' : ''}`} onDoubleClick={toggleSidebar}>
+          <div className="content">
+            {SidebarData.map((item) => {
+              let toRender;
+
+              if (item.tabName.startsWith("separator")) {
+                toRender = <div key={item.link} className="separator"></div>
+              } else {
+                toRender = <Link key={item.link} to={item.link}><span className="material-symbols-outlined">{item.icon}</span><span className="text">{item.tabName}</span></Link>
+              }
+
+              return toRender;
+            })}
+          </div>
+          <div className="bottom-content">
+            <div className="separator"></div>
+            <Link to="#envelope"><span className="material-symbols-outlined">account_circle</span><span className="text">UserName</span></Link>
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
