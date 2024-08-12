05d3eb9e3f3e20eb886457587908183189507769
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-08-12 13:49:44 +0530

    [breadcrumb] added

---
+++ react-navbar-sidebar-richtext-layout-31/src/components/breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,46 @@
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import "./Breadcrumb.css";
+import { memo } from 'react';
+
+const Breadcrumb = memo(() => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  
+  const pathnames = location.pathname.split('/').filter((x) => x);
+
+  const handleClick = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <nav className="crumbs">
+      <ol>
+        {pathnames.length > 0 ? (
+          <li className="crumb">
+            <Link to="" onClick={() => handleClick('/')}>Home</Link>
+          </li>
+        ) : (
+          <li className="crumb">
+            Home
+          </li>
+        )}
+        {pathnames.map((value, index) => {
+          const isLast = index === pathnames.length - 1;
+          const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+
+          return isLast ? (
+            <li key={to} className="crumb">
+              {value}
+            </li>
+          ) : (
+            <li key={to} className="crumb">
+              <Link to="" onClick={() => handleClick(to)}>{value}</Link>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  )
+});
+
+export default Breadcrumb;
